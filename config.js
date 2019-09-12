6c30de83d604a419b8ebbd91aa745385935767c7
Gene Smith <me@genesmith.org>
2019-09-11 20:56:08 -0400

    initial commit

---
+++ config.js
@@ -0,0 +1,37 @@
+module.exports = {
+  siteTitle: 'IndieGoodz Coming Soon', // <title>
+  manifestName: 'Eventually',
+  manifestShortName: 'Landing', // max 12 characters
+  manifestStartUrl: '/',
+  manifestBackgroundColor: '#663399',
+  manifestThemeColor: '#663399',
+  manifestDisplay: 'standalone',
+  manifestIcon: 'src/assets/img/website-icon.png',
+  pathPrefix: `/gatsby-starter-eventually/`, // This path is subpath of your hosting https://domain/portfolio
+  // social
+  heading: 'IndieGoodz',
+  subHeading:
+    "Coming Soon",
+  socialLinks: [
+    {
+      icon: 'fa-github',
+      name: 'Github',
+      url: 'https://github.com/anubhavsrivastava',
+    },
+    {
+      icon: 'fa-twitter',
+      name: 'Twitter',
+      url: 'https://twitter.com/onlyanubhav',
+    },
+    {
+      icon: 'fa-facebook',
+      name: 'Facebook',
+      url: 'https://facebook.com/theanubhav',
+    },
+    {
+      icon: 'fa-envelope-o',
+      name: 'Email',
+      url: 'mailto:anubhav.srivastava00@gmail.com',
+    },
+  ],
+};
---
+++ src/components/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import config from '../../config';
+
+export default function Footer() {
+  return (
+    <footer id="footer">
+      <ul className="copyright">
+        <li>&copy;2019 IndieGoodz</li>
+      </ul>
+    </footer>
+  );
+}
